4c0691972eb8bfc9baa4a14f59d7111beef6ae73
John <83322224+J-The-Dev@users.noreply.github.com>
2022-04-19 14:43:36 -0500

    Update projects on homepage

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ const Home = () => {
 				<Text title="A little about me" text="Hi, I'm John. I'm a 7th grader that is interested in tech, software development, internet privacy, and crypto. I am currently most interested in front-end web development and cryptocurrencies/decentralization but plan on expanding my skill set. I also enjoy playing violin with orchestra, tennis, and math (especially algebra). You can find all of my current projects and what I'm doing <a href='https://github.com/J-The-Dev'>here</a>."/>
 				<Image image={ Hobbies }/>
 				<Image image={ Projects }/>
-				<Text title="Projects" text="My projects include: Canvas+ a chrome extension for the school website canvas that I made with <a href='https://github.com/adriancasares'>Adrian Casares</a> that adds features such as dark mode, search, and a lot more. Crypto Prices is another chrome extension that makes it easy to quickly check crypto prices, its features include dark mode, useful links, and even a 7-day graph. More projects coming soon..."/>
+				<Text title="Projects" text="My projects include: Canvas+ a chrome extension for the school website canvas that I made with <a href='https://github.com/adriancasares'>Adrian Casares</a> that adds features such as dark mode, search, and a lot more. Crypto Prices is another chrome extension that makes it easy to quickly check crypto prices, its features include dark mode, useful links, search, and even a 7-day graph. More projects coming soon..."/>
 			</div>
 			<BottomDivider className={ styles.divider } style={{ background: "linear-gradient(white 90%, #313131)" }} viewBox="0 0 11 1" width="100%" height="100%"/>
 			<Footer/>
