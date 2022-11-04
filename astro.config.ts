e1250e30f74e3e24cd2a9044e52cda287623897f
John <jlozier99@gmail.com>
2022-11-04 13:15:39 -0500

    remove SPA

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [
-    SolidJS(),
-    tailwind()
-  ]
+	integrations: [
+		SolidJS(),
+		tailwind()
+	]
 });
