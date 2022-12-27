429c4b71d5449f058232fb042b0492262b4311b1
JohnLozier <jlozier99@gmail.com>
2022-12-27 15:39:23 -0600

    fix link

--- src/components/construction.tsx
+++ src/components/construction.tsx
@@ -7,7 +7,7 @@ const Construction = () => {
 				<h1 class="text-4xl  mt-8 font-montserrat font-bo	ld text-center text-gray-700">Sorry we are down for maintenance</h1>
 				<h3 class="text-1.5xl mt-8 font-opensans text-center mx-10 text-gray-500">We should be back up in a month of two better than before, so you won't have to wait long</h3>
 				<div class="text-20xl self-center -mt-20">🚧</div>
-				<h3 class="text-1.5xl mt-5 font-opensans text-center mx-5 text-gray-500">In the meantime you can go check out my new project <Link class="text-blue-700 duration-300 hover:opacity-60 opacity-50" href="/codetyper">CodeTyper</Link> or my <Link class="text-blue-700 hover:opacity-60 transition-opacity duration-300 opacity-50" href="https://github.com/JohnLozier">Github</Link>!</h3>
+				<h3 class="text-1.5xl mt-5 font-opensans text-center mx-5 text-gray-500">In the meantime you can go check out my new project <a class="text-blue-700 duration-300 hover:opacity-60 opacity-50" href="/codetyper">CodeTyper</a> or my <Link class="text-blue-700 hover:opacity-60 transition-opacity duration-300 opacity-50" href="https://github.com/JohnLozier">Github</Link>!</h3>
 			</div>
 		</div>
 	);
